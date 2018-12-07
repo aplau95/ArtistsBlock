77cca9b4ebe9f35017c4eea995743dc20c075550
Andrew Lau <andrewlau@MacBook-Pro.local>
2018-12-06 19:29:44 -0800

    asdf

--- app/src/main/java/com/fireapp/andrewlau/myapplication/GenerateActivity.kt
+++ app/src/main/java/com/fireapp/andrewlau/myapplication/GenerateActivity.kt
@@ -1,39 +1,64 @@
 package com.fireapp.andrewlau.myapplication
 
-import android.app.Activity
+
 import android.content.Intent
 import android.content.res.Configuration
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.FragmentManager
-import android.support.v7.app.AppCompatActivity
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.graphics.*
+import android.net.Uri
 import android.support.v7.graphics.Palette
-import android.util.Log
-import com.fireapp.andrewlau.myapplication.ImageList.imageList
-import kotlinx.android.synthetic.main.activity_main.*
-import android.widget.Toast
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
+import android.widget.LinearLayout
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_swatch.*
 
 
-class MainActivity : Activity(){
+//import edu.calpoly.rscovil.dialogfragments.OpenFinishFrag
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class GenerateActivity : Fragment(){
     val unsplashApiLink = "https://api.unsplash.com/photos/random?client_id=bc37901814227d25f99ac03155e0e111c458e496b2d2bd6a14dab630e5568247&count=30&orientation=landscape&featured"
-    val manager = SupportFra
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_swatch)
-        val fm = fra
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_swatch, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val getImage = getView()!!.findViewById(R.id.NextImage) as Button
+        val imageDisplay = getView()!!.findViewById(R.id.imageDisplay) as ImageView
         GetImageAsyncTask(::updateInfo).execute(unsplashApiLink)
 
-        CompleteButton.setOnClickListener {
+
+//        val currentOrientation = resources.configuration.orientation
+//        if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+//            println("LANDSCAPE")
+//        } else {
+//            // Portrait
+//        }
+
+        val completeButton = getView()!!.findViewById(R.id.CompleteButton) as Button
+        completeButton.setOnClickListener {
             openFinishFragment()
+
         }
 
-        NextImage.setOnClickListener() {
+        getImage.setOnClickListener() {
             if (ImageList.imageList.size < 3){
                 GetImageAsyncTask(::updateInfo).execute(unsplashApiLink)
             } else {
@@ -45,6 +70,30 @@ class MainActivity : Activity(){
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(ImageList.currentImage))
             startActivity(browserIntent)
         }
+
+        var isImageFitToScreen = false
+//        imageDisplay.setOnClickListener(){
+//            if (isImageFitToScreen) {
+//                isImageFitToScreen = false
+//                imageDisplay.layoutParams =
+//                        LinearLayout.LayoutParams(
+//                            LinearLayout.LayoutParams.WRAP_CONTENT,
+//                            LinearLayout.LayoutParams.WRAP_CONTENT
+//
+//                        )
+//                imageDisplay.adjustViewBounds = true
+//            } else {
+//                isImageFitToScreen = true
+//                imageDisplay.layoutParams =
+//                        LinearLayout.LayoutParams(
+//                            LinearLayout.LayoutParams.MATCH_PARENT,
+//                            LinearLayout.LayoutParams.MATCH_PARENT
+//                        )
+//                imageDisplay.scaleType = ImageView.ScaleType.FIT_XY
+//            }
+//        }
+
+
     }
 
 
@@ -111,7 +160,6 @@ class MainActivity : Activity(){
 
     private fun openFinishFragment() {
         val dialog = OpenFinishFrag()
-        val fm = su
-        dialog.show(fm, "name")
+        dialog.show(fragmentManager, "NoticeDialogFragment")
     }
 }
